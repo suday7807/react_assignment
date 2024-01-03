a7714607ee0d1fbfb8d4e91c85e319c24ccf686d
Uday Singh <suday7807@gmail.com>
2024-01-04 02:33:38 +0530

    "done"

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,10 +2,19 @@ import React, { useContext } from "react";
 import { Context } from "./CartContext";
 import Navbar from "./Navbar";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
+  const navigate = useNavigate();
   const { cart, setCart } = useContext(Context);
 
+  const handleCheckout = () => {
+    setCart([]);
+    toast("Your order is placed");
+    setTimeout(() => {
+      navigate("/home");
+    }, 2000);
+  };
   const handleRemove = (id) => {
     const arr = cart.filter((item) => item.id != id);
     setCart(arr);
@@ -52,7 +61,7 @@ const Cart = () => {
                 Total: ${calculateTotal()}
               </p>
               <button
-                onClick={() => toast("Your order is placed")}
+                onClick={handleCheckout}
                 className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center"
               >
                 Check Out
